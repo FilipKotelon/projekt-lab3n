3a996affa53a3062985d1887b4099a583a000a9d
FilipKotelon <48619142+FilipKotelon@users.noreply.github.com>
2019-06-16 21:08:45 +0200

    Project upload

---
+++ GraFlight/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GraFlight
+{
+    class Program
+    {
+        int score = 0;
+        public static char[] playerLine = new char[] { ' ', ' ', ' ', ' ', 'O', ' ', ' ', ' ', ' ' };
+
+        public static char[] MovePlayer(System.Windows.Forms.Keys KeyUsed)
+        {
+            return new char[] { };
+        }
+
+        public static string DisplayLine(char[] Line)
+        {
+            string LineString = String.Empty;
+            foreach(char el in Line)
+            {
+                LineString += el;
+            }
+            return LineString;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DisplayLine(playerLine));
+        }
+    }
+}
